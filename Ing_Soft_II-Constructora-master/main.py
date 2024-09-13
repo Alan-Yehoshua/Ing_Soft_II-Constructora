8bffe25766d7b33d1fc01b6d382bbffeaa320641
Alan-Yehoshua <Allen_west@outlook.com>
2024-09-13 07:47:18 -0600

    Cell to Input

--- Ing_Soft_II-Constructora-master/main.py
+++ Ing_Soft_II-Constructora-master/main.py
@@ -14,14 +14,23 @@ class HomeWindow(QMainWindow):
 
         # Inicializa la conexión a Supabase
         self.supabase: Client = create_client("https://qlsmhahdkovvqfauntde.supabase.co", "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InFsc21oYWhka292dnFmYXVudGRlIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MjYwNjcxMjIsImV4cCI6MjA0MTY0MzEyMn0.K-LSXODTvSbvFqu6hnJWYiP8KDmm959LZB5xYQl6aPI")
-
+        self.loadCustomerData()
+        self.TableCustomer.cellClicked.connect(self.cellsToInputs)
+        
     def customerPage(self):
         self.stackedWidget.setCurrentIndex(0)
         self.loadCustomerData()
 
     def employePage(self):
         self.stackedWidget.setCurrentIndex(1)
-
+        
+    def cellsToInputs(self, fila, columna):
+        # Obtener los datos de la fila seleccionada y transferirlos a los QLineEdit
+        self.NameCustomer.setText(self.TableCustomer.item(fila, 1).text())
+        self.LastNamePaCustomer.setText(self.TableCustomer.item(fila, 2).text())
+        self.AddressCustomer.setText(self.TableCustomer.item(fila, 3).text())
+        self.NumberCustomer.setText(self.TableCustomer.item(fila, 4).text())
+        
     def loadCustomerData(self):
         # Obtiene los datos de la tabla de clientes
         response = self.supabase.table("cliente").select("*").execute()
