293066634a710642d365e00c2c88010471dd78bd
Alan-Yehoshua <Allen_west@outlook.com>
2024-09-13 07:39:37 -0600

    Cell to Input

--- main.py
+++ main.py
@@ -1,23 +1,53 @@
-from PyQt5.QtWidgets import QMainWindow, QApplication
+from PyQt5.QtWidgets import QMainWindow, QApplication, QTableWidgetItem
 from PyQt5.uic import loadUi
 import sys
+from supabase import create_client, Client
+
+#BASE DE DATOS DE ALAN
+url = "https://njgqpwaocrkghoaetovp.supabase.co"
+key = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im5qZ3Fwd2FvY3JrZ2hvYWV0b3ZwIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MjU1ODY0MDYsImV4cCI6MjA0MTE2MjQwNn0.t-WxD9PtP-oZBvxMGiWmuENSPU_e9nD81B9ima1dOlU"
 
 class HomeWindow(QMainWindow):
     def __init__(self):
         super(HomeWindow, self).__init__()
         loadUi("HomePage.ui", self)
-        
+
         self.CustomerButton.clicked.connect(self.customerPage)
         self.EmployeButton.clicked.connect(self.employePage)
+        # Inicializa la conexión a Supabase  BASE DE DATOS DE VICTOR
+        self.supabase: Client = create_client("https://qlsmhahdkovvqfauntde.supabase.co", "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InFsc21oYWhka292dnFmYXVudGRlIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MjYwNjcxMjIsImV4cCI6MjA0MTY0MzEyMn0.K-LSXODTvSbvFqu6hnJWYiP8KDmm959LZB5xYQl6aPI")
+        self.loadCustomerData()
+        
+        self.TableCustomer.cellClicked.connect(self.transferir_datos_a_inputs)
+        
+    def transferir_datos_a_inputs(self, fila, columna):
+        # Obtener los datos de la fila seleccionada y transferirlos a los QLineEdit
+        self.NameCustomer.setText(self.TableCustomer.item(fila, 1).text())
+        self.LastNamePaCustomer.setText(self.TableCustomer.item(fila, 2).text())
+        self.AddressCustomer.setText(self.TableCustomer.item(fila, 3).text())
+        self.NumberCustomer.setText(self.TableCustomer.item(fila, 4).text())
         
     def customerPage(self):
         self.stackedWidget.setCurrentIndex(0)
-        
+        self.loadCustomerData()
+
     def employePage(self):
         self.stackedWidget.setCurrentIndex(1)
-        
+
+    def loadCustomerData(self):
+        # Obtiene los datos de la tabla de clientes
+        response = self.supabase.table("cliente").select("*").execute()
+
+        # Asumiendo que tienes una QTableWidget llamada customerTable
+        self.TableCustomer.setRowCount(0)
+        for row, data in enumerate(response.data):
+            self.TableCustomer.insertRow(row)
+            for col, (key, value) in enumerate(data.items()):
+                self.TableCustomer.setItem(row, col, QTableWidgetItem(str(value)))
+
+
 if __name__ == "__main__":
     app = QApplication(sys.argv)
     ui = HomeWindow()
     ui.show()
-    sys.exit(app.exec_()) 
+    sys.exit(app.exec_())
